1eb7416fe2e3e3499c6ccbb2a0fef145d353a92e
Olimjon-04042000 <nishonovolimjon2000@gmail.com>
2024-03-15 21:26:42 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+// import Result from "./components/Result";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="w-full max-w-[730px] mt-[44px] mx-auto">
+      <Header />
+      <Search />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <div className=" flex justify-between mb-9 ">
+      <a className=" text-[#222731] font-bold text-[26px]" href="">devfinder</a>
+      <p className=" flex gap-4 items-center text-[#697C9A] font-bold text-[13px] tracking-[2.5px] ">DARK <img src="/moon.svg" alt="moon-icon" /></p>
+      </div>
+  )
+}
+
+export default Header
